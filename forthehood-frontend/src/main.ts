21deb761ee444b1f95f58f5616b2b385e8b2ce06
lambaotran3044 <lambaotran34@gmail.com>
2026-03-17 13:31:13 +0700

    ket noi backend thanh cong

---
+++ forthehood-frontend/src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css"
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+    <h1 class="text-4xl font-bold text-purple-600">
+      Forthehood Shop
+    </h1>
+  </div>
+`
